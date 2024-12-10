c68a6153257efdda597071c7d2138784daf6e516
guole <guole.fun@qq.com>
2024-12-10 17:09:23 +0800

    Fix copy package.json

--- copyPackageJson.js
+++ copyPackageJson.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+const publicDir = path.join(__dirname, 'public');
 const source = path.join(__dirname, 'node_modules', 'guole.fun.api', 'package.json');
-const destination = path.join(__dirname, 'public', 'guole.fun.api.package.json');
+const destination = path.join(publicDir, 'guole.fun.api.package.json');
 
+// 确保 public 目录存在
+if (!fs.existsSync(publicDir)) {
+  fs.mkdirSync(publicDir);
+}
+
+// 复制 package.json
 fs.copyFile(source, destination, (err) => {
   if (err) {
     console.error('Error copying package.json:', err);
