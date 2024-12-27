4e5ba066ec2454ad9641e0e672f5c264719bf982
guole <guole.fun@qq.com>
2024-12-27 18:04:26 +0800

    🏖️ API更新于 2024年12月27日 18:04:26

--- copyPackageJson.js
+++ copyPackageJson.js
@@ -6,15 +6,14 @@ const source = path.join(__dirname, 'node_modules', 'guole.fun.api', 'package.js
 const destination = path.join(publicDir, 'guole.fun.api.package.json');
 
 // 确保 public 目录存在
-if (!fs.existsSync(publicDir)) {
-  fs.mkdirSync(publicDir);
-}
-
-// 复制 package.json
-fs.copyFile(source, destination, (err) => {
-  if (err) {
-    console.error('Error copying package.json:', err);
-  } else {
+fs.promises.mkdir(publicDir, { recursive: true })
+  .then(() => {
+    // 复制 package.json
+    return fs.promises.copyFile(source, destination);
+  })
+  .then(() => {
     console.log('guole.fun.api package.json was copied to public/');
-  }
-});
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+  });
