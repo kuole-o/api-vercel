90b5c8062cc66d19838c5f10ae0b5e2aa9f0923f
郭乐 <63436555+kuole-o@users.noreply.github.com>
2024-12-28 19:10:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-import express from "express";
+//import express from "express";
 import serveHotApi from "guole.fun.api";
-import { ipAddress, geolocation, json } from '@vercel/edge';
+//import { ipAddress, geolocation, json } from '@vercel/edge';
 
-const app = express();
+//const app = express();
 
 // /ip 接口逻辑
-app.get('/ip', (req, res) => {
-  const ip = ipAddress(req);
-  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
-  console.log(ip, pathname);
-  const geo = geolocation(req);
-  console.log(geo);
-  res.json({
-    ip,
-    ...geo
-  }, {
-    headers: {
-      'x-client-ip': ip
-    }
-  });
-});
+//app.get('/ip', (req, res) => {
+  //const ip = ipAddress(req);
+  //const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+  //console.log(ip, pathname);
+  //const geo = geolocation(req);
+  //console.log(geo);
+  //res.json({
+    //ip,
+   // ...geo
+ // }, {
+   // headers: {
+    //  'x-client-ip': ip
+   // }
+  //});
+//});
 
 // 其他 API 路由
-app.use(serveHotApi());
+//app.use(serveHotApi());
 
-//serveHotApi()
+serveHotApi()
 
-export default app;s
+//export default app;s
