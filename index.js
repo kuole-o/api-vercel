e0a61d4c78984fa4e1a3702e2dc2126d4674a5c5
guole <guole.fun@qq.com>
2024-12-28 17:43:43 +0800

    🏖️ API更新于 2024年12月28日 17:43:43

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-import express from "express";
 import serveHotApi from "guole.fun.api";
 
-const app = express();
-
-app.use("/*.png", express.static("public"));
-app.use("/*.ico", express.static("public/ico"));
-app.use("/package.json", express.static("guole.fun.api.package.json"));
-
-app.use(serveHotApi());
-
-export default app;
+serveHotApi()
